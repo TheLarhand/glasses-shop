b310caba2d1611a2e1e8859601bd1b914b3db62d
Игорь Соколов <@itstep.krg>
2024-04-25 18:14:18 +0500

    start work at sort panel

---
+++ src/components/shop/filters/SortPanel.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './SortPanel.module.css'
+
+const SortPanel = ({}) => {
+    return (
+        <div className={classes.wrapper}>
+            
+        </div>
+    );
+}
+
+export default SortPanel;
